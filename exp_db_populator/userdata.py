abdb12d5a14098709c28622a6b751753b71258df
Dominic Oram <dominic.oram@stfc.ac.uk>
2018-10-02 15:36:54 +0100

    Added more tests

--- exp_db_populator/userdata.py
+++ exp_db_populator/userdata.py
@@ -2,6 +2,9 @@ from database_model import Role, User
 
 
 class UserData:
+    """
+    A class for holding all the data required for a row in the experiment team table.
+    """
     def __init__(self, name, organisation, role, rb_number, start_date):
         self.name = name
         self.organisation = organisation
@@ -15,16 +18,20 @@ class UserData:
     @property
     def user_id(self):
         """
-        Get the id associated with a user, this will add a new id to the database if one doesn't exist.
-        :return: The id of the user
+        Gets the user id for the user. Will create an entry for the user in the database if one doesn't exist.
+
+        Returns: the user's id.
+
         """
         return User.get_or_create(name=self.name, organisation=self.organisation)[0].userid
 
     @property
     def role_id(self):
         """
-        Get the role_id associated with the user's role.
-        :return: The role id of the user
+        Gets the role id for the user based on the roles in the database. Will raise an exception if role is not found.
+
+        Returns: the role id.
+
         """
         return Role.get(Role.name == self.role).roleid
 
---
+++ tests/userdata_tests.py
@@ -0,0 +1,56 @@
+import unittest
+import exp_db_populator.database_model as model
+from peewee import SqliteDatabase, PeeweeException
+from webservices_test_data import *
+from exp_db_populator.userdata import UserData
+
+
+class UserDataTests(unittest.TestCase):
+
+    def setUp(self):
+        # Creates an in-memory database for testing
+        database = SqliteDatabase(":memory:")
+        model.database_proxy.initialize(database)
+        model.database_proxy.create_tables([model.User, model.Experimentteams, model.Experiment, model.Role])
+        self.user_data = UserData(TEST_PI_NAME, TEST_PI_ORG, TEST_PI_ROLE, TEST_RBNUMBER,   TEST_DATE)
+
+    def test_GIVEN_empty_database_WHEN_user_id_requested_THEN_user_created(self):
+        self.assertEqual(0, model.User.select().count())
+
+        self.user_data.user_id
+
+        users = model.User.select()
+        self.assertEqual(1, users.count())
+        self.assertEqual(TEST_PI_NAME, users[0].name)
+        self.assertEqual(TEST_PI_ORG, users[0].organisation)
+
+    def test_GIVEN_user_exists_WHEN_user_id_requested_THEN_no_new_user_created(self):
+        model.User.create(name=TEST_PI_NAME, organisation=TEST_PI_ORG)
+        self.assertEqual(1, model.User.select().count())
+
+        self.user_data.user_id
+
+        users = model.User.select()
+        self.assertEqual(1, users.count())
+        self.assertEqual(TEST_PI_NAME, users[0].name)
+        self.assertEqual(TEST_PI_ORG, users[0].organisation)
+
+    def test_WHEN_non_existant_role_id_requested_THEN_exception_thrown_and_no_new_role_created(self):
+        self.assertEqual(0, model.Role.select().count())
+
+        try:
+            self.user_data.role_id
+            self.assertTrue(False)
+        except model.Role.DoesNotExist as e:
+            self.assertTrue(True)
+
+        self.assertEqual(0, model.Role.select().count())
+
+    def test_WHEN_pre_existing_role_id_requested_THEN_role_id_returned_and_no_new_role_created(self):
+        existing_role = model.Role.create(name=TEST_PI_ROLE, priority=1)
+        self.assertEqual(1, model.Role.select().count())
+
+        role_id = self.user_data.role_id
+
+        self.assertEqual(1,  model.Role.select().count())
+        self.assertEqual(existing_role.roleid, role_id)
