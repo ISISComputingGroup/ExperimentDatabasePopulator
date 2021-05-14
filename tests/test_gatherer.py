9799b3628eee2113cbb76c91056ab35c95374515
Sam Jenkins <s.jenkins@stfc.ac.uk>
2021-05-14 15:45:35 +0100

    fixed accidental duplicate test

--- tests/test_gatherer.py
+++ tests/test_gatherer.py
@@ -58,20 +58,6 @@ class GathererTests(unittest.TestCase):
 
         update.assert_not_called()
 
-    @patch('exp_db_populator.gatherer.update')
-    @patch('exp_db_populator.gatherer.gather_data')
-    def test_GIVEN_instrument_list_has_unscheduled_instrument_WHEN_gatherer_started_THEN_no_update(self, gather_data,
-                                                                                                   update):
-        new_name, new_host = "TEST", "NDXTEST"
-        inst_list = [{"name": new_name, "hostName": new_host, "isScheduled": False}]
-        gather_data.return_value = []
-
-        new_gatherer = Gatherer(inst_list, self.lock, False)
-        new_gatherer.start()
-        new_gatherer.join()
-
-        update.assert_not_called()
-
     def test_GIVEN_data_of_correct_instrument_WHEN_filter_called_THEN_data_accepted(self):
         inst_name = "TEST_INSTRUMENT"
         data_item = {'instrument': inst_name}
