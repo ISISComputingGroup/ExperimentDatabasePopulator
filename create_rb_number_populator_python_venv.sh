ff93eb9c156bc0726e5dc4844dd63e75a10b2c2c
JackEAllen <jack.allen@stfc.ac.uk>
2021-11-16 13:41:43 +0000

    Fix variable declaration

--- create_rb_number_populator_python_venv.sh
+++ create_rb_number_populator_python_venv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This script should be executed in as a SU from within /home/epics/RB_num_populator/
-$venv="exp_db_populator_venv" # Name of the virtual environment
+venv="exp_db_populator_venv" # Name of the virtual environment
 /usr/local/bin/python3.8 -m venv /home/epics/RB_num_populator/$venv # create virtual environment
 source $venv/bin/activate # activate the virtual environment
 $venv/bin/pip install -r requirements.txt # Install requirements.txt
