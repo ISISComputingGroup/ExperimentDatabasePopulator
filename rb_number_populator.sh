697e9c60a877da6db8846d0a3bd570d26bdbbae8
RaiBishal <55101160+RaiBishal@users.noreply.github.com>
2021-11-17 15:25:34 +0000

    Update rb_number_populator.sh

--- rb_number_populator.sh
+++ rb_number_populator.sh
@@ -3,5 +3,5 @@ venv="exp_db_populator_venv" # Name of the virtual environment
 
 . /home/epics/EPICS/config_env.sh
 source /home/epics/RB_num_populator/$venv/bin/activate # activate the virtual environment
-$venv/bin/python3.8 /home/epics/RB_num_populator/main.py
+/home/epics/RB_num_populator/$venv/bin/python3.8 /home/epics/RB_num_populator/main.py
 deactivate # deactivate the virtual environment
